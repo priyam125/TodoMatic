c7f58c4ecb656beca0fad9eb75b07c15a5a94d44
priyam125 <sinha.priyam002@gmail.com>
2024-01-18 12:35:06 +0530

    persistant data on reload

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,22 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import todoReducer from "./todos/todoReducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 const store = configureStore({
   reducer: {
-    todos: todoReducer,
+    todos: persistedReducer,
   },
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
